f2bb7e5c2fbc339b436c308fcf865a7b3c1e63b4
dvir <dvirm.biton@gmail.com>
2025-05-28 17:50:24 +0300

    Added circular buffer data structure

---
+++ system/data_structures/include/circular_buffer/circular_buffer.h
@@ -0,0 +1,26 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 256
+
+typedef struct circular_buffer_t {
+    char *data;
+    size_t capacity;
+    size_t count;
+    size_t head;
+    size_t tail;
+} circular_buffer_t;
+
+// init
+void cb_init(circular_buffer_t *cb, size_t size);
+
+// write
+bool cb_write(circular_buffer_t *cb, char data);
+
+// read
+bool cb_read(circular_buffer_t *cb, char *out); 
+
+#endif // CIRCULAR_BUFFER_H
---
+++ system/data_structures/src/circular_buffer/circular_buffer.c
@@ -0,0 +1,37 @@
+#include "circular_buffer/circular_buffer.h"
+
+void cb_init(circular_buffer_t *cb, size_t size) {
+    cb->capacity = size;
+    // should be used when we get an heap
+    // cb->data = (char*) kmalloc(size * sizeof(char));
+
+    cb->head = 0;
+    cb->tail = 0;
+    cb->count = 0;
+}
+
+bool cb_write(circular_buffer_t *cb, char data) {
+    if (cb->count == cb->capacity) {
+        return false;
+    }
+
+    cb->data[cb->head] = data;
+
+    cb->head = (cb->head + 1) % cb->capacity;
+    cb->count++;
+    
+    return true;
+}
+
+bool cb_read(circular_buffer_t *cb, char *out) {
+    if (cb->capacity == 0 || cb->tail == cb->head) {
+        return false;
+    }
+
+    *out = cb->data[cb->tail];
+
+    cb->tail = (cb->tail + 1) % cb->capacity;
+    cb->count--;
+
+    return true;
+}
