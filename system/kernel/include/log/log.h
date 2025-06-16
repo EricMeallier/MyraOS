349b3c0a54ad0aa555d1959bed3ed8c146aed4e0
dvir-biton <dvirm.biton@gmail.com>
2025-06-16 10:38:25 +0300

    Log: Fix missing import

--- system/kernel/include/log/log.h
+++ system/kernel/include/log/log.h
@@ -1,8 +1,10 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define klog_info(msg) printf("INFO: %s\n", msg)
-#define klog_warn(msg) printf("WARN: %s\n", msg)
-#define klog_error(msg) printf("ERROR: %s\n", msg)
+#include <print/print.h>
+
+#define klog_info(msg) kprintf("INFO: %s\n", msg)
+#define klog_warn(msg) kprintf("WARN: %s\n", msg)
+#define klog_error(msg) kprintf("ERROR: %s\n", msg)
 
 #endif // LOG_H
