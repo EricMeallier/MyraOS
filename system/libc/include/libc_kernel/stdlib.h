77b95479fc3e09bbf91780c517c000bca4c91537
dvir-biton <dvirm.biton@gmail.com>
2025-06-17 09:18:03 +0300

    STD: Add itoa padding

--- system/libc/include/libc_kernel/stdlib.h
+++ system/libc/include/libc_kernel/stdlib.h
@@ -1,6 +1,8 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 
-int atoi(const char* str);
+int katoi(const char* str);
+
+char* kitoa(int value, char* str, int base);
 
 #endif // STDLIB_H
--- system/libc/src/libc_kernel/stdlib.c
+++ system/libc/src/libc_kernel/stdlib.c
@@ -1,6 +1,8 @@
 #include "libc_kernel/stdlib.h"
 
-int atoi(const char* s) {
+#include <stdbool.h>
+
+int katoi(const char* s) {
     const int INT_MAX = 2147483647, INT_MIN = -2147483647;
     int sign = 1, res = 0, idx = 0;
 
@@ -22,4 +24,42 @@ int atoi(const char* s) {
     }
 
     return res * sign;
+}
+
+void kitoa(int value, char* str, int base) {
+    char* ptr = str;
+    char* ptr1 = str;
+    char tmp_char;
+    int tmp_val;
+
+    if (value == 0) {
+        *ptr++ = '0';
+        *ptr = '\0';
+        return;
+    }
+
+    bool is_negative = false;
+    if (value < 0 && base == 10) {
+        is_negative = true;
+        value = -value;
+    }
+
+    while (value != 0) {
+        tmp_val = value % base;
+        *ptr++ = (tmp_val < 10) ? (tmp_val + '0') : (tmp_val - 10 + 'a');
+        value /= base;
+    }
+
+    if (is_negative)
+        *ptr++ = '-';
+
+    *ptr = '\0';
+
+    // Reverse string
+    ptr--;
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr-- = *ptr1;
+        *ptr1++ = tmp_char;
+    }
 }
