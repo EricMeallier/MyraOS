d00063d99b133c711e8431187a7786daa3ecea78
dvir-biton <dvirm.biton@gmail.com>
2025-08-03 09:54:23 +0300

    feat(screen): Add top bar widget to the desktop screen

--- system/graphics/src/screen/screen.c
+++ system/graphics/src/screen/screen.c
@@ -1,6 +1,7 @@
 #include "screen/screen.h"
 
 #include "font/fonts/myra_font.h"
+#include "os_widgets/top_bar_widget.h"
 #include "ui/widgets/label_widget.h"
 #include "ui/ui.h"
 
@@ -10,5 +11,6 @@ screen_t screen_desktop = {
 };
 
 void screen_desktop_init(void) {
+    ui_add_widget(top_bar_create(0xFF010101, 0, 0, 40));
     ui_add_widget(label_create(50, 50, "Welcome to MyraOS! hi!", &myra_font, 0xFFFFFFFF));
 }
