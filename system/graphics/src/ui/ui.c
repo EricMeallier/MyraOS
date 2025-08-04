bb741aa336932fea4b8c0f7bd2dd6c848cf80b93
dvir-biton <dvirm.biton@gmail.com>
2025-08-04 16:47:02 +0300

    update(ui): Change ui to support the structre changes

--- system/graphics/src/ui/ui.c
+++ system/graphics/src/ui/ui.c
@@ -2,6 +2,8 @@
 
 #include <stddef.h>
 
+#include "heap/heap.h"
+
 static widget_t* ui_widgets[MAX_WIDGETS];
 static size_t ui_widget_count = 0;
 
@@ -43,7 +45,7 @@ void ui_render(void) {
     }
 }
 
-void ui_handle_click(int x, int y) {
+void ui_handle_click(uint32_t x, uint32_t y) {
     for (int i = ui_widget_count - 1; i >= 0; i--) {
         widget_t* w = ui_widgets[i];
         if (!w->visible || !w->on_click) {
@@ -51,8 +53,8 @@ void ui_handle_click(int x, int y) {
         }
 
         if (x >= w->x && x < w->x + w->width && y >= w->y && y < w->y + w->height) {
-            int rel_x = x - w->x;
-            int rel_y = y - w->y;
+            uint32_t rel_x = x - w->x;
+            uint32_t rel_y = y - w->y;
 
             w->on_click(w, rel_x, rel_y);
             
