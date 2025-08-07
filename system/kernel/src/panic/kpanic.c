00c28f76983002313ce7609531a8fa0b517a18ae
dvir-biton <dvirm.biton@gmail.com>
2025-08-07 12:06:04 +0300

    feat(panic): Add a new GUI panic screen

--- system/kernel/src/panic/kpanic.c
+++ system/kernel/src/panic/kpanic.c
@@ -4,8 +4,22 @@
 
 #include "interrupt/isr/isr.h"
 #include "print/print.h"
+#include "gfx/gfx.h"
+#include "font/font.h"
+#include "mouse/mouse.h"
 
 void kpanic(const char* message) {
+    mouse_set(false);
+    for (uint32_t layer = 0; layer < LAYER_COUNT; layer++) {
+        gfx_clear(layer, 0x00000000);
+    }
+    gfx_clear(LAYER_BACKGROUND, 0xFFFF6961);
+
+    font_restore_default();
+    font_writef("Kernel panic! Please provide this error and reset: %s", message);
+
+    gfx_flush();
+
     kclear_screen();
     kset_color(COLOR_RED);
 
